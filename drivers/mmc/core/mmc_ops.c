8175a412d7803ee1d8c50677a23c6c292da1d64a
Ryan QIAN <b32804@freescale.com>
2013-01-16 10:23:28 +0800

    MLK-11685-1 mmc: add delay after CMD6 befoer sending CMD13 for sandisk
    - Some sandisk emmc cards need certain delay befor sending cmd13 after cmd6.
    Original CR: ENGR174296 (commit: fd031f9)
    
    Acked-by: Aisheng Dong <b29396@freescale.com>
    Signed-off-by: Ryan QIAN <b32804@freescale.com>
    (cherry picked from commit f942bf1db36355d46f38792601594949f3f2c71b)
    Signed-off-by: Haibo Chen <haibo.chen@freescale.com>
    
    Conflicts:
    	drivers/mmc/core/mmc_ops.c
    [Quanyang:Original patch taken from
    https://source.codeaurora.org/external/imx/linux-imx.git imx_4.9.88_2.0.0_ga]
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- drivers/mmc/core/mmc_ops.c
+++ drivers/mmc/core/mmc_ops.c
@@ -573,6 +573,11 @@ int __mmc_switch(struct mmc_card *card, u8 set, u8 index, u8 value,
 	/* No need to check card status in case of unblocking command */
 	if (!use_busy_signal)
 		goto out;
+	/*
+	 * WORKAROUND: for Sandisk eMMC cards, it might need certain delay
+	 * before sending CMD13 after CMD6
+	 */
+	mdelay(1);
 
 	/*If SPI or used HW busy detection above, then we don't need to poll. */
 	if (((host->caps & MMC_CAP_WAIT_WHILE_BUSY) && use_r1b_resp) ||
