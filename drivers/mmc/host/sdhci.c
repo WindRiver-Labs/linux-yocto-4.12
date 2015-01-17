a152fadfbf2ecb5c906c2f2f1b69b71a19a9a032
Marcin Wojtas <mw@semihalf.com>
2015-01-18 00:02:58 +0100

    mmc: sdhci: add init_card callback to sdhci
    commit  4f01de73d56f41595324e7f5f839116dc8a8d4dd from
    https://github.com/MarvellEmbeddedProcessors/linux-marvell.git
    
    Some sdhci hosts may require handling quirks during card initialization at
    the time when its type is already known. Hence a new callback (init_card)
    is added in sdhci_ops.
    
    Change-Id: Ibbfbcd0289aebae4fb1d0a5a622069c393a71ca9
    Signed-off-by: Marcin Wojtas <mw@semihalf.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/27300
    Reviewed-by: Nadav Haklai <nadavh@marvell.com>
    Tested-by: Lior Amsalem <alior@marvell.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -2287,6 +2287,14 @@ static void sdhci_card_event(struct mmc_host *mmc)
 	spin_unlock_irqrestore(&host->lock, flags);
 }
 
+static void sdhci_init_card(struct mmc_host *mmc, struct mmc_card *card)
+{
+	struct sdhci_host *host = mmc_priv(mmc);
+
+	if (host->ops->init_card)
+		host->ops->init_card(host, card);
+}
+
 static const struct mmc_host_ops sdhci_ops = {
 	.request	= sdhci_request,
 	.post_req	= sdhci_post_req,
@@ -2301,6 +2309,7 @@ static const struct mmc_host_ops sdhci_ops = {
 	.execute_tuning			= sdhci_execute_tuning,
 	.card_event			= sdhci_card_event,
 	.card_busy	= sdhci_card_busy,
+	.init_card	= sdhci_init_card,
 };
 
 /*****************************************************************************\
