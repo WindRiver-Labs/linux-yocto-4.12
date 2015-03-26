9c7c5f57bba107eb312e63816934174d58c97de7
Nikhil Badola <nikhil.badola@freescale.com>
2015-03-26 09:14:00 +0530

    drivers:usb:dwc3 : Implement workaround for Erratum A009116
    Write fladj register adjusts (micro)frame length to appropriate
    value thus avoiding USB 2.0 devices to time-out over a longer run
    
    Signed-off-by: Nikhil Badola <nikhil.badola@freescale.com>
    Change-Id: I26df8b7d134d08171a096dba7871f7334be02315
    Reviewed-on: http://git.am.freescale.net:8181/33664
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Ramneek Mehresh <ramneek.mehresh@freescale.com>
    Reviewed-by: Honghua Yin <Hong-Hua.Yin@freescale.com>
    [Original patch taken from QorIQ-SDK-V2.0-20160527-yocto]
    Signed-off-by: Yanjiang Jin <yanjiang.jin@windriver.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -994,6 +994,8 @@ static void dwc3_get_properties(struct dwc3 *dwc)
 				&hird_threshold);
 	dwc->usb3_lpm_capable = device_property_read_bool(dev,
 				"snps,usb3_lpm_capable");
+	dwc->configure_gfladj = device_property_read_bool(dev,
+				"configure-gfladj");
 
 	dwc->disable_scramble_quirk = device_property_read_bool(dev,
 				"snps,disable_scramble_quirk");
@@ -1170,6 +1172,11 @@ static int dwc3_probe(struct platform_device *pdev)
 		ret = -ENOMEM;
 		goto err2;
 	}
+	
+	/* Adjust Frame Length */
+	if (dwc->configure_gfladj)
+		dwc3_writel(dwc->regs, DWC3_GFLADJ, GFLADJ_30MHZ_REG_SEL |
+			    GFLADJ_30MHZ(GFLADJ_30MHZ_DEFAULT));
 
 	ret = dwc3_get_dr_mode(dwc);
 	if (ret)
