cd07f6cad03a032dbaa88f067be2dc57c423a08e
Victor Gu <xigu@marvell.com>
2016-09-02 00:35:47 +0800

    PCI: Aardvark: check bus number for config read and write
    commit  a87571efa36563a06648fcd27a8c02b89b85db26 from
    https://github.com/MarvellEmbeddedProcessors/linux-marvell.git
    
    The none zero devices are illegal only if it is on root bus.
    And when issue PIO read and write operation, the bus information
    should also be included.
    
    Change-Id: I82b572ce89b5289764fd642da303d0b4378e616c
    Signed-off-by: Victor Gu <xigu@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/32440
    Reviewed-by: Wilson Ding <dingwei@marvell.com>
    Tested-by: Star_Automation <star@marvell.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/pci/host/pci-aardvark.c
+++ drivers/pci/host/pci-aardvark.c
@@ -442,7 +442,7 @@ static int advk_pcie_rd_conf(struct pci_bus *bus, u32 devfn,
 	u32 reg;
 	int ret;
 
-	if (PCI_SLOT(devfn) != 0) {
+	if ((bus->number == pcie->root_bus_nr) && (PCI_SLOT(devfn) != 0)) {
 		*val = 0xffffffff;
 		return PCIBIOS_DEVICE_NOT_FOUND;
 	}
@@ -461,7 +461,7 @@ static int advk_pcie_rd_conf(struct pci_bus *bus, u32 devfn,
 	advk_writel(pcie, reg, PIO_CTRL);
 
 	/* Program the address registers */
-	reg = PCIE_BDF(devfn) | PCIE_CONF_REG(where);
+	reg = PCIE_CONF_ADDR(bus->number, devfn, where);
 	advk_writel(pcie, reg, PIO_ADDR_LS);
 	advk_writel(pcie, 0, PIO_ADDR_MS);
 
@@ -496,7 +496,7 @@ static int advk_pcie_wr_conf(struct pci_bus *bus, u32 devfn,
 	int offset;
 	int ret;
 
-	if (PCI_SLOT(devfn) != 0)
+	if ((bus->number == pcie->root_bus_nr) && (PCI_SLOT(devfn) != 0))
 		return PCIBIOS_DEVICE_NOT_FOUND;
 
 	if (where % size)
