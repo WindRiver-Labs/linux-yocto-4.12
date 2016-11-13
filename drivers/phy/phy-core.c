b7851caac162d085ede96e54886452fa72a93fb2
Igal Liberman <igall@marvell.com>
2016-11-13 15:44:21 +0200

    phy: add phy_get_mode callback
    commit  676c3e58d66bad502a18abed8b6d532a493df965 from
    https://github.com/MarvellEmbeddedProcessors/linux-marvell.git
    
    get_mode call back will allow phy consumer to be aware of the
    operation mode of a specific phy.
    
    Change-Id: I5b92e05e661bd37b94fc382943d4482bb0e378a5
    Signed-off-by: Igal Liberman <igall@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/33723
    Reviewed-by: Omri Itach <omrii@marvell.com>
    Tested-by: Omri Itach <omrii@marvell.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/phy/phy-core.c
+++ drivers/phy/phy-core.c
@@ -372,6 +372,21 @@ int phy_reset(struct phy *phy)
 }
 EXPORT_SYMBOL_GPL(phy_reset);
 
+enum phy_mode phy_get_mode(struct phy *phy)
+{
+	int ret;
+
+	if (!phy || !phy->ops->get_mode)
+		return 0;
+
+	mutex_lock(&phy->mutex);
+	ret = phy->ops->get_mode(phy);
+	mutex_unlock(&phy->mutex);
+
+	return ret;
+}
+EXPORT_SYMBOL_GPL(phy_get_mode);
+
 /**
  * _of_phy_get() - lookup and obtain a reference to a phy by phandle
  * @np: device_node for which to get the phy
