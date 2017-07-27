f98ee7b8452ad2385bc3ee0d806510fe1bfeb6bf
Michal Simek <michal.simek@xilinx.com>
2017-07-27 10:34:22 +0200

    gpu: drm: xilinx: Remove blank lines
    There is no need for additional blank lines.
    All reported by checkpatch.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    (cherry picked from commit e7c1aa1752f8c606e57c8142efc4eab5be89ca4c)
    Signed-off-by: Zumeng Chen <zumeng.chen@windriver.com>

--- drivers/gpu/drm/xilinx/xilinx_drm_dp_sub.c
+++ drivers/gpu/drm/xilinx/xilinx_drm_dp_sub.c
@@ -384,7 +384,6 @@ xilinx_drm_dp_sub_blend_layer_enable(struct xilinx_drm_dp_sub_blend *blend,
 			  XILINX_DP_SUB_V_BLEND_LAYER_CONTROL + layer->offset,
 			  reg);
 
-
 	if (layer->id == XILINX_DRM_DP_SUB_LAYER_VID)
 		offset = XILINX_DP_SUB_V_BLEND_IN1CSC_COEFF0;
 	else
@@ -1344,7 +1343,6 @@ xilinx_drm_dp_sub_layer_get(struct xilinx_drm_dp_sub *dp_sub, bool primary)
 		return ERR_PTR(-ENODEV);
 
 	return layer;
-
 }
 EXPORT_SYMBOL_GPL(xilinx_drm_dp_sub_layer_get);
 
