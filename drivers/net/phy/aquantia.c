3475e0882f307f56adb9c50277e72be201e34426
Limeng <Meng.Li@windriver.com>
2017-08-25 13:20:41 +0800

    driver : pyh: delete driver field initialization in struct phy_driver
    In linux kernel 4.8, there is no the field of driver in
    struct phy_driver, so delete driver field initialization
    like others phy driver definition.
    
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/net/phy/aquantia.c
+++ drivers/net/phy/aquantia.c
@@ -190,7 +190,6 @@ static struct phy_driver aquantia_driver[] = {
 	.config_intr	= aquantia_config_intr,
 	.ack_interrupt	= aquantia_ack_interrupt,
 	.read_status	= aquantia_read_status,
-	.driver		= { .owner = THIS_MODULE,},
 },
 {
 	.phy_id		= PHY_ID_AQR107,
@@ -203,7 +202,6 @@ static struct phy_driver aquantia_driver[] = {
 	.config_intr	= aquantia_config_intr,
 	.ack_interrupt	= aquantia_ack_interrupt,
 	.read_status	= aquantia_read_status,
-	.driver		= { .owner = THIS_MODULE,},
 },
 {
 	.phy_id		= PHY_ID_AQR405,
