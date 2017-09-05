de9338d0bf3bf5c73546b33503d8bfc99bded99b
Dong Aisheng <aisheng.dong@nxp.com>
2017-09-05 20:35:16 +0800

    MLK-17491-8 pinctrl: imx: remove gpio_request_enable and gpio_disable_free
    gpio_request_enable/disable_free actually are not quite necessary as
    standard IMX pinctrl binding already sets GPIO mux from device tree,
    e.g. VF610_PAD_PTB20__GPIO_42 or MX7D_PAD_SD2_CD_B__GPIO5_IO9
    No need to do it again in gpio_request_enable.
    
    Fixes: 5d7a13220ade ("MLK-15128-3 pinctrl: freescale: support scu and memmap pinctrl together")
    Fixes: 07787c40ff3b ("MLK-13485-3 pinctrl: imx: modify the imx pinctrl to support imx7ulp gpio")
    Cc: Fugang Duan <fugang.duan@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    Signed-off-by: Dong Aisheng <aisheng.dong@nxp.com>
    [Quanyang:Original patch taken from
    https://source.codeaurora.org/external/imx/linux-imx.git imx_4.9.88_2.0.0_ga]
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- drivers/pinctrl/freescale/pinctrl-imx.c
+++ drivers/pinctrl/freescale/pinctrl-imx.c
@@ -195,30 +195,6 @@ static int imx_pmx_set(struct pinctrl_dev *pctldev, unsigned selector,
 	return 0;
 }
 
-static int imx_pmx_gpio_request_enable(struct pinctrl_dev *pctldev,
-			struct pinctrl_gpio_range *range, unsigned offset)
-{
-	struct imx_pinctrl *ipctl = pinctrl_dev_get_drvdata(pctldev);
-	const struct imx_pinctrl_soc_info *info = ipctl->info;
-
-	if (info->flags & IMX8_USE_SCU)
-		return imx_pmx_backend_gpio_request_enable_scu(pctldev, range, offset);
-	else
-		return imx_pmx_backend_gpio_request_enable_mem(pctldev, range, offset);
-}
-
-static void imx_pmx_gpio_disable_free(struct pinctrl_dev *pctldev,
-			struct pinctrl_gpio_range *range, unsigned offset)
-{
-	struct imx_pinctrl *ipctl = pinctrl_dev_get_drvdata(pctldev);
-	const struct imx_pinctrl_soc_info *info = ipctl->info;
-
-	if (info->flags & IMX8_USE_SCU)
-		imx_pmx_backend_gpio_disable_free_scu(pctldev, range, offset);
-	else
-		imx_pmx_backend_gpio_disable_free_mem(pctldev, range, offset);
-}
-
 static int imx_pmx_gpio_set_direction(struct pinctrl_dev *pctldev,
 	   struct pinctrl_gpio_range *range, unsigned offset, bool input)
 {
@@ -238,8 +214,6 @@ static const struct pinmux_ops imx_pmx_ops = {
 	.get_function_name = pinmux_generic_get_function_name,
 	.get_function_groups = pinmux_generic_get_function_groups,
 	.set_mux = imx_pmx_set,
-	.gpio_request_enable = imx_pmx_gpio_request_enable,
-	.gpio_disable_free = imx_pmx_gpio_disable_free,
 	.gpio_set_direction = imx_pmx_gpio_set_direction,
 };
 
