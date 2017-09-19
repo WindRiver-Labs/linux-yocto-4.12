cc36d96e1a97556235840c72e5a8bc8a4c4e94fb
Ioana Radulescu <ruxandra.radulescu@nxp.com>
2017-09-19 14:21:41 +0300

    staging: fsl-dpaa2/eth: Fix NAPI initialization
    Make sure the NAPI instances are enabled only after all other
    device initialization is done, to avoid accessing uninit vars.
    
    Mirror that change on device remove routine.
    
    Signed-off-by: Ioana Radulescu <ruxandra.radulescu@nxp.com>
    [Xulin: Original patch taken from NXP lx2160a-early-access-bsp0.3.]
    Signed-off-by: Xulin Sun <xulin.sun@windriver.com>

--- drivers/staging/fsl-dpaa2/ethernet/dpaa2-eth.c
+++ drivers/staging/fsl-dpaa2/ethernet/dpaa2-eth.c
@@ -3343,10 +3343,6 @@ static int dpaa2_eth_probe(struct fsl_mc_device *dpni_dev)
 	if (err)
 		goto err_bind;
 
-	/* Add a NAPI context for each channel */
-	add_ch_napi(priv);
-	enable_ch_napi(priv);
-
 	/* Percpu statistics */
 	priv->percpu_stats = alloc_percpu(*priv->percpu_stats);
 	if (!priv->percpu_stats) {
@@ -3393,6 +3389,10 @@ static int dpaa2_eth_probe(struct fsl_mc_device *dpni_dev)
 	net_dev->dcbnl_ops = &dpaa2_eth_dcbnl_ops;
 	priv->dcbx_mode = DCB_CAP_DCBX_HOST | DCB_CAP_DCBX_VER_IEEE;
 #endif
+
+	/* Add a NAPI context for each channel */
+	add_ch_napi(priv);
+	enable_ch_napi(priv);
 
 	err = setup_irqs(dpni_dev);
 	if (err) {
@@ -3456,6 +3456,9 @@ static int dpaa2_eth_remove(struct fsl_mc_device *ls_dev)
 #endif
 	dpaa2_eth_sysfs_remove(&net_dev->dev);
 
+	disable_ch_napi(priv);
+	del_ch_napi(priv);
+
 	unregister_netdev(net_dev);
 	dev_info(net_dev->dev.parent, "Removed interface %s\n", net_dev->name);
 
@@ -3467,9 +3470,6 @@ static int dpaa2_eth_remove(struct fsl_mc_device *ls_dev)
 	free_rings(priv);
 	free_percpu(priv->percpu_stats);
 	free_percpu(priv->percpu_extras);
-
-	disable_ch_napi(priv);
-	del_ch_napi(priv);
 	free_dpbp(priv);
 	free_dpio(priv);
 	free_dpni(priv);
