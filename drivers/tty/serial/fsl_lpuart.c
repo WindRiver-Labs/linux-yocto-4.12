415dab474250f2b8cc473a48e2a8cf7a628f7b9d
Abel Vesa <abel.vesa@nxp.com>
2017-11-27 18:10:11 +0200

    MLK-17005: linux-imx: serial: fsl_lpuart: Clear LOOP mode when requested
    The LOOP mode remained always set after first use.
    If the ioctl tiocmset gets called with TIOCMBIC for TIOCM_LOOP,
    UARTCTRL_LOOPS needs to be cleared in the LPUART control register.
    
    Signed-off-by: Abel Vesa <abel.vesa@nxp.com>
    Acked-by: Fugang Duan <fugang.duan@nxp.com>
    [Quanyang:Original patch taken from
    https://source.codeaurora.org/external/imx/linux-imx.git imx_4.9.88_2.0.0_ga]
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- drivers/tty/serial/fsl_lpuart.c
+++ drivers/tty/serial/fsl_lpuart.c
@@ -1185,6 +1185,9 @@ static void lpuart32_set_mctrl(struct uart_port *port, unsigned int mctrl)
 	temp = lpuart32_read(port->membase + UARTCTRL);
 	if (mctrl & TIOCM_LOOP)
 		temp |= UARTCTRL_LOOPS;
+	else
+		temp &= ~UARTCTRL_LOOPS;
+
 	lpuart32_write(temp, port->membase + UARTCTRL);
 }
 
