9f86199c2bdba04e1a32e1a088a8dc3b1c4968a1
Zumeng Chen <zumeng.chen@windriver.com>
2018-03-12 18:29:09 +0800

    net: macb: Fix kernel crash when MACB_EXT_BD is enabled
    When MACB_EXT_BD config option is enabled, while sending TCP data, observing
    Kernel crash due to NULL pointer dereference.
    
    Added fix to ensure skb is a valid pointer before dereferencing it for
    checking the time stamp option.
    
    Signed-off-by: Punnaiah Choudary Kalluri <punnaia@xilinx.com>
    Acked-by: Harini Katakam <harinik@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Zumeng Chen <zumeng.chen@windriver.com>

--- drivers/net/ethernet/cadence/macb.c
+++ drivers/net/ethernet/cadence/macb.c
@@ -770,12 +770,12 @@ static void macb_tx_interrupt(struct macb_queue *queue)
 					    skb->data);
 				bp->dev->stats.tx_packets++;
 				bp->dev->stats.tx_bytes += skb->len;
+#ifdef CONFIG_MACB_EXT_BD
+				if (skb_shinfo(skb)->tx_flags & SKBTX_HW_TSTAMP)
+					macb_handle_txtstamp(bp, skb, desc);
+#endif
 			}
 
-#ifdef CONFIG_MACB_EXT_BD
-			if (skb_shinfo(skb)->tx_flags & SKBTX_HW_TSTAMP)
-				macb_handle_txtstamp(bp, skb, desc);
-#endif
 			/* Now we can safely release resources */
 			macb_tx_unmap(bp, tx_skb);
 
