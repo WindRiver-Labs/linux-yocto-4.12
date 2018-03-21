59a5339dacefb6b28d4fac49ed8b6447fe80df7b
Haibo Chen <haibo.chen@nxp.com>
2018-03-21 18:26:46 +0800

    MLK-17883 mmc: add delay after CMD6 before sending CMD13
    Android report data timeout issue on the Micron eMMC of i.mx8mscale
    evk board. Before using CMD25 to write multi block data, driver will
    use CMD6 to flush cache. But some CMD25 will get both data timeout
    interrupt and transfer complete interrupt. If add delay after busy
    check, and before sending CMD13, this issue gone.
    
    We meet similar issue on Sandisk eMMC before, and already have a
    commit 581925fe325a ("MLK-11685-1 mmc: add delay after CMD6 befoer
    sending CMD13 for sandisk"), but when cherry-pick to 4.9 branch,
    meet conflicts, put the delay in the wrong place. So meet the similar
    issue again.
    
    This patch fix this, move the dealy in the right place, right before
    CMD13.
    
    Reviewed-by: Dong Aisheng <Aisheng.dong@nxp.com>
    Signed-off-by: Haibo Chen <haibo.chen@nxp.com>
    [Quanyang:Original patch taken from
    https://source.codeaurora.org/external/imx/linux-imx.git imx_4.9.88_2.0.0_ga]
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- drivers/mmc/core/mmc_ops.c
+++ drivers/mmc/core/mmc_ops.c
@@ -573,11 +573,6 @@ int __mmc_switch(struct mmc_card *card, u8 set, u8 index, u8 value,
 	/* No need to check card status in case of unblocking command */
 	if (!use_busy_signal)
 		goto out;
-	/*
-	 * WORKAROUND: for Sandisk eMMC cards, it might need certain delay
-	 * before sending CMD13 after CMD6
-	 */
-	mdelay(1);
 
 	/*If SPI or used HW busy detection above, then we don't need to poll. */
 	if (((host->caps & MMC_CAP_WAIT_WHILE_BUSY) && use_r1b_resp) ||
@@ -593,6 +588,12 @@ out_tim:
 	/* Switch to new timing before check switch status. */
 	if (timing)
 		mmc_set_timing(host, timing);
+
+	/*
+	 * WORKAROUND: for Sandisk eMMC cards, it might need certain delay
+	 * before sending CMD13 after CMD6
+	 */
+	mdelay(1);
 
 	if (send_status) {
 		err = mmc_switch_status(card);
