1f9ff2a9bf906a12983c1c2d8fab1bfaad1f10e9
Zumeng Chen <zumeng.chen@windriver.com>
2018-06-28 14:20:30 +0800

    remoteproc: add debugfs entry to print the firmware version info
    This commit comes from:
      git://git.ti.com/processor-sdk/processor-sdk-linux.git
    
    A debugfs entry called "version" is created under the corresponding
    remoteproc directory when the firmware image is processed. The
    design relies on the firmware file having a special section
    containing the version info. The version info has to be stored into
    this section in the remote processor image build step.
    
    This is currently implemented for ELF images by storing the version
    information in a specific ELF section named ".version".
    
    A read on the entry will print the contents of the version section.
    The following shows an example usage:
        cat /sys/kernel/debug/remoteproc/remoteproc0/version
    
    This is based on a patch developed by Juan Gutierrez on 3.4 vendor
    kernel, and has been reworked and ported to current kernel.
    
    Signed-off-by: Juan Gutierrez <jgutierrez@ti.com>
    Signed-off-by: Suman Anna <s-anna@ti.com>
    Signed-off-by: Zumeng Chen <zumeng.chen@windriver.com>

--- drivers/remoteproc/remoteproc_core.c
+++ drivers/remoteproc/remoteproc_core.c
@@ -855,6 +855,20 @@ static void rproc_remove_subdevices(struct rproc *rproc)
 		subdev->remove(subdev);
 }
 
+/* handle firmware version entry before loading the firmware sections */
+static int
+rproc_handle_fw_version(struct rproc *rproc, const char *version, int versz)
+{
+	struct device *dev = &rproc->dev;
+
+	rproc->fw_version = kmemdup(version, versz, GFP_KERNEL);
+	if (!rproc->fw_version) {
+		dev_err(dev, "%s: version kzalloc failed\n", __func__);
+		return -ENOMEM;
+	}
+	return 0;
+}
+
 /**
  * rproc_resource_cleanup() - clean up and free all acquired resources
  * @rproc: rproc handle
@@ -902,13 +916,17 @@ static void rproc_resource_cleanup(struct rproc *rproc)
 	/* clean up remote vdev entries */
 	list_for_each_entry_safe(rvdev, rvtmp, &rproc->rvdevs, node)
 		kref_put(&rvdev->refcount, rproc_vdev_release);
+
+	/* free fw version */
+	kfree(rproc->fw_version);
 }
 
 static int rproc_start(struct rproc *rproc, const struct firmware *fw)
 {
 	struct resource_table *table, *loaded_table;
 	struct device *dev = &rproc->dev;
-	int ret, tablesz;
+	int ret, tablesz, versz;
+	const char *version;
 
 	/* look for the resource table */
 	table = rproc_find_rsc_table(rproc, fw, &tablesz);
@@ -1017,6 +1035,17 @@ static int rproc_fw_boot(struct rproc *rproc, const struct firmware *fw)
 	if (ret) {
 		dev_err(dev, "Failed to process resources: %d\n", ret);
 		goto clean_up_resources;
+	}
+
+	/* look for the firmware version, and store if present */
+	version = rproc_find_version_info(rproc, fw, &versz);
+	if (version) {
+		ret = rproc_handle_fw_version(rproc, version, versz);
+		if (ret) {
+			dev_err(dev, "Failed to process version info: %d\n",
+				ret);
+			goto clean_up_resources;
+		}
 	}
 
 	ret = rproc_start(rproc, fw);
