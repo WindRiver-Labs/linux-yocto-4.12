Xilinx/linux-xlnx

qspi: Added support for is25lp256d in kernel with quad read and write operation.
IS25LP256D supports quad read and write operations unlike IS25CD512 which only supports dual read/ write.
Added support for enabling quad en bit for the flash part.

Signed-off-by: Tejas Prajapati Rameshchandra <tejaspra@xilinx.com>
Signed-off-by: Michal Simek <michal.simek@xilinx.com>

include/linux/mtd/spi-nor.h
@@ -28,6 +28,7 @@
 #define SNOR_MFR_SPANSION	CFI_MFR_AMD
 #define SNOR_MFR_SST		CFI_MFR_SST
 #define SNOR_MFR_WINBOND	0xef /* Also used by some Spansion */
+#define SNOR_MFR_ISSI		CFI_MFR_ISSI
 
 /*
  * Note on opcode nomenclature: some opcodes have a format like

WindRiver-Labs/linux-yocto-4.12

(cherry picked from commit d0c8401a6bda672ac9bf8d09db848417009aba38)
Signed-off-by: Zumeng Chen <zumeng.chen@windriver.com>

include/linux/mtd/spi-nor.h
@@ -28,6 +28,7 @@
 #define SNOR_MFR_SPANSION	CFI_MFR_AMD
 #define SNOR_MFR_SST		CFI_MFR_SST
 #define SNOR_MFR_WINBOND	0xef /* Also used by some Spansion */
+#define SNOR_MFR_ISSI		CFI_MFR_PMC
 
 /*
  * Note on opcode nomenclature: some opcodes have a format like